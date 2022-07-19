680d8acdf3afbcb466f09d3fd12aa1392c0022d6
Shinji <shinji@shinji.rocks>
2022-07-19 18:53:33 +0800

    just some updates on emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,6 @@
+(setq gc-cons-threshold 100000000)
 (setq user-mjolnir-directory (concat (getenv "HOME") "/.mjolnir.d/"))
 
 (org-babel-load-file
  (expand-file-name "config.org" user-mjolnir-directory))
+(setq gc-cons-threshold 800000)
